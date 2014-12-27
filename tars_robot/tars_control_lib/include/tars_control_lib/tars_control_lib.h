d9bd9348d364fc21ed3a832c04480d5a8707bbb5
David Brody <dbrody@gmail.com>
2014-12-27 00:07:50 -0800

    Adds geting joint positions to c++ tars_control_lib

--- tars_robot/tars_control_lib/include/tars_control_lib/tars_control_lib.h
+++ tars_robot/tars_control_lib/include/tars_control_lib/tars_control_lib.h
@@ -2,8 +2,10 @@
 #ifndef TARS_CONTROL_LIB_H
 #define TARS_CONTROL_LIB_H
 
+#include <math.h>
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
+#include <sensor_msgs/JointState.h>
 #include <cassert>
 
 class TarsControlLib {
@@ -22,6 +24,7 @@ public:
 	}
 
 	static bool joint1(double value){
+		value = fmod(value, M_PI * 2);
 		ROS_INFO("Publishing to Joint1: %f", value);
 		std_msgs::Float64 msg;
 		msg.data = value;
@@ -29,6 +32,7 @@ public:
 	}
 
 	static bool joint2(double value){
+		value = fmod(value, M_PI * 2);
 		ROS_INFO("Publishing to Joint2: %f", value);
 		std_msgs::Float64 msg;
 		msg.data = value;
@@ -36,10 +40,42 @@ public:
 	}
 
 	static bool joint3(double value){
+		value = fmod(value, M_PI * 2);
 		ROS_INFO("Publishing to Joint3: %f", value);
 		std_msgs::Float64 msg;
 		msg.data = value;
 		get()->pubJoint3.publish(msg);
+	}
+
+	static double joint1(){
+		return fmod(get()->j1pos, M_PI * 2);
+	}
+
+	static double joint2(){
+		return fmod(get()->j2pos, M_PI * 2);
+	}
+
+	static double joint3(){
+		return fmod(get()->j3pos, M_PI * 2);
+	}
+
+	static void jointStateCallback(const sensor_msgs::JointState& msg){
+		TarsControlLib* inst = get();
+		for(unsigned int i = 0; i < msg.name.size(); i++){
+			if(msg.name[i].compare("joint1") == 0){
+				inst->j1pos = msg.position[i];
+				inst->j1vel = msg.velocity[i];
+				inst->j1effort = msg.effort[i];
+			} else if(msg.name[i].compare("joint2") == 0){
+				inst->j2pos = msg.position[i];
+				inst->j2vel = msg.velocity[i];
+				inst->j2effort = msg.effort[i];
+			} else if(msg.name[i].compare("joint3") == 0){
+				inst->j3pos = msg.position[i];
+				inst->j3vel = msg.velocity[i];
+				inst->j3effort = msg.effort[i];
+			}
+		}
 	}
 
 private:
@@ -49,6 +85,8 @@ private:
 		pubJoint1 = nh_->advertise<std_msgs::Float64>("/tars/joint1_position_controller/command", 10);
 		pubJoint2 = nh_->advertise<std_msgs::Float64>("/tars/joint2_position_controller/command", 10);
 		pubJoint3 = nh_->advertise<std_msgs::Float64>("/tars/joint3_position_controller/command", 10);
+
+		subTarsJointState = nh->subscribe("/tars/joint_states", 10, TarsControlLib::jointStateCallback);
 	};
 
 	static TarsControlLib* _inst;
@@ -61,6 +99,20 @@ private:
 	ros::Publisher pubJoint1;
 	ros::Publisher pubJoint2;
 	ros::Publisher pubJoint3;
+
+	ros::Subscriber subTarsJointState;
+
+	/* Model Parameters / State */
+
+	// Joint Positions
+	double j1pos, j2pos, j3pos;
+
+	// Joint Velocities
+	double j1vel, j2vel, j3vel;
+
+	// Joint Effort
+	double j1effort, j2effort, j3effort;
+
 };
 
 #endif
